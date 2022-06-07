887632337511928df2c3ac22861b2b2697131311
sato <ksato100430066@gmail.com>
2022-06-08 00:00:20 +0900

    initial commit

---
+++ __init__.py
@@ -0,0 +1,26 @@
+from finance_client.csv.client import CSVClient, MultiFrameClient
+import finance_client.coincheck
+from finance_client.frames import Frame
+from finance_client.client_base import ClientBase
+
+
+available_clients = {
+    CSVClient.kinds : CSVClient,
+    MultiFrameClient.kinds: MultiFrameClient
+}
+
+def client_to_params(client):
+    params = {}
+    params["kinds"] = client.kinds
+    params["args"] = client.args
+    return params
+
+def load_client(params:dict):
+    kinds = params["kinds"]
+    if kinds in available_clients:
+        args = params["args"]
+        Client = available_clients[kinds]
+        data_client = Client(*args)
+        return data_client
+    return None
+    
---
+++ position.py
@@ -0,0 +1,10 @@
+class Position:
+    
+    def __init__(budget):
+        pass
+    
+    def open_position():
+        pass
+    
+    def close_position():
+        pass
