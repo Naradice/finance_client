9575153b0a4c80d19853a567100c8c6b97e07716
Naradice <ksato100430066@gmail.com>
2026-01-24 12:34:17 +0900

    fixed unintented change

--- src/finance_client/sbi/sbi_util/sbi_util/utils.py
+++ src/finance_client/sbi/sbi_util/sbi_util/utils.py
@@ -13,8 +13,8 @@ def setup_logger():
 
     log_dir = "logs"
     os.makedirs(log_dir, exist_ok=True)
-    file_handler.setLevel(logging.INFO)
     file_handler = logging.FileHandler(os.path.join(log_dir, "rpa.log"))
+    file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     return logger
